d24751ee48d3acef9d7fa9f29a047c5c2dd7b260
Connor Hansen <chansen13@georgefox.edu>
2021-11-02 09:20:27 -0700

    merge DBTest w/ working db.
    Co-authored by Anna Clarke: <annac.klingberg@gmail.com>

---
+++ GardenAPI/Data/SeedSeeder.cs
@@ -0,0 +1,44 @@
+using GardenAPI.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.IO;
+
+
+namespace GardenAPI.Data
+{
+  public class SeedSeeder
+  {
+    private readonly GardenAPIContext _db;
+    private readonly IWebHostEnvironment _env;
+    public SeedSeeder(GardenAPIContext db, IWebHostEnvironment env)
+    {
+      _db = db;
+      _env = env;
+    }
+    private string GetData()
+    {
+      string rootPath = _env.ContentRootPath;
+      string filePath = Path.GetFullPath(Path.Combine(rootPath, "Data", "seed_data.json"));
+      using (var r = new StreamReader(filePath))
+      {
+        string json = r.ReadToEnd();
+        return json;
+      }
+    }
+    public void Seed()
+    {
+      string data = GetData();
+      var items = JsonSerializer.Deserialize<List<Dictionary<string, dynamic>>>(data);
+      foreach (var item in items)
+      {
+        var s = new Seed(item["seedname"], item["sqfootplant"], item["daystillharvest"],
+                        item["waterinterval"], item["daystillsprout"], item["companions"],
+                        item["enemies"]);
+        _db.Seeds.Add(s);
+      }
+      _db.SaveChanges();
+    }
+  }
+
+}
--- GardenAPI/Models/Seed.cs
+++ GardenAPI/Models/Seed.cs
@@ -2,6 +2,9 @@ namespace GardenAPI.Models
 {
   public class Seed
   {
+    public Seed()
+    {
+    }
     public int SeedId { get; set; }
     public string SeedName { get; set; }
     public int SqFootPlant { get; set; }
@@ -10,5 +13,17 @@ namespace GardenAPI.Models
     public int DaysTillSprout { get; set; }
     public string Companions { get; set; }
     public string Enemies { get; set; }
+
+    public Seed(string seedname, int sqfootplant, int daystillharvest, int waterinterval,
+               int daystillsprout, string companions, string enemies)
+    {
+      SeedName = seedname;
+      SqFootPlant = sqfootplant;
+      DaysTillHarvest = daystillharvest;
+      WaterInterval = waterinterval;
+      DaysTillSprout = daystillsprout;
+      Companions = companions;
+      Enemies = enemies;
+    }
   }
 }
